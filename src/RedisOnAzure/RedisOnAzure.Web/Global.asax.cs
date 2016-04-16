12b6c1342200ae3254a349a3cccb186550e6426d
Brian Hall <brian@nebbiatech.com>
2016-04-16 11:11:40 -0400

    Removed some server headers for security

--- src/RedisOnAzure/RedisOnAzure.Web/Global.asax.cs
+++ src/RedisOnAzure/RedisOnAzure.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -19,6 +20,15 @@ namespace RedisOnAzure.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Removes header from response that identifies this as an asp.net application (security risk)
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+
+
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            Context.Response.Headers.Remove("Server");
         }
     }
 }
