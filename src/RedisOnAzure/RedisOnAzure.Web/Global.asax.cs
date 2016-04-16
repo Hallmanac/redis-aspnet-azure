eba8cc87cf72a9bfafd92a6e838e8962d87bdda3
Brian Hall <brian@nebbiatech.com>
2016-04-16 11:08:16 -0400

    Added functionality for Redis Session State Provider.

--- src/RedisOnAzure/RedisOnAzure.Web/Global.asax.cs
+++ src/RedisOnAzure/RedisOnAzure.Web/Global.asax.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace RedisOnAzure.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            // Setup the Session state to use Redis and point to the appropriate Redis server
+            var webConfigPath = Server.MapPath("/web.config");
+            RedisSessionStateConfig.InitializeSessionStateConfiguration(webConfigPath);
+
+            // Standard ASP.NET stuff
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
